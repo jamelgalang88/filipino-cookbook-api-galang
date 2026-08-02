0306ed2093d33fec90d01ff380181d8ed27e6daa
Jamel Galang <130065893+jamelgalang88@users.noreply.github.com>
2026-08-03 07:23:39 +1300

    Add files via upload

---
+++ client/config.example.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'api_base_url' => 'http://localhost/filipino-cookbook-api-cuares-main/public',
+    'api_token' => 'YOUR_ACCESS_TOKEN',
+    'api_developer' => 'Cuares, John Mark Perez',
+];
---
+++ client/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'api_base_url' => 'http://localhost/filipino-cookbook-api-cuares-main/public',
+    'api_token' => 'dmmmsu-cookbook-token-2026',
+    'api_developer' => 'Cuares, John Mark Perez',
+];
