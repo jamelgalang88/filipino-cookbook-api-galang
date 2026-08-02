2a3fe57d38a7c4bdf298dd0009c82285cb119f60
Jamel Galang <130065893+jamelgalang88@users.noreply.github.com>
2026-08-03 07:25:45 +1300

    Delete client directory

--- client/config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'api_base_url' => 'http://localhost/filipino-cookbook-api-cuares-main/public',
-    'api_token' => 'YOUR_ACCESS_TOKEN',
-    'api_developer' => 'Cuares, John Mark Perez',
-];
